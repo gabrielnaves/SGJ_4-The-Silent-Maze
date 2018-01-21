f4244c45b73413c8c1b3cb0fdf00e69884165bc1
Gabriel Naves da Silva <gabri.navess@gmail.com>
2018-01-21 08:56:34 -0200

    Starting enemy implementation, created a navmesh

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour {
+
+    public Transform footprintContainer;
+    public AudioClip[] footstepSounds;
+    public AudioSource footstepSource;
+    public float footstepTime = 0.5f;
+
+    float elapsedTime;
+    bool move;
+    NavMeshAgent agent;
+
+    void Awake() {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void FixedUpdate() {
+        CheckNoiseLevel();
+        if (move)
+            MoveTowardsPlayer();
+    }
+
+    void CheckNoiseLevel() {
+        int noiseLevel = Player.instance.noiseLevel;
+        if (noiseLevel > 0)
+            move = true;
+    }
+
+    void MoveTowardsPlayer() {
+        agent.SetDestination(Player.instance.transform.position);
+        UpdateFootsteps();
+    }
+
+    void UpdateFootsteps() {
+        elapsedTime += Time.fixedDeltaTime;
+        if (elapsedTime > footstepTime) {
+            PlayFootstepSound();
+            elapsedTime = 0;
+        }
+    }
+
+    void PlayFootstepSound() {
+        // TODO
+    }
+}
